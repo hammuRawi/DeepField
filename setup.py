84e947b52525d05dc09d9361aa1515dcc1420bfa
hammuRawi <89481208+hammuRawi@users.noreply.github.com>
2021-12-13 02:48:30 +0300

    update python==3.9

--- setup.py
+++ setup.py
@@ -58,6 +58,7 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9,
         'Topic :: Scientific/Engineering'
     ],
 )
